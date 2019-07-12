d32f17506aca7427397f3ceb54e0f125df24d0c5
MariusParasca <mrs.parasca@gmail.com>
2019-07-12 09:39:15 +0300

    better way to make conditional shows

--- src/App.js
+++ src/App.js
@@ -40,12 +40,31 @@ class App extends Component {
 
   render() {
     const style = {
-      
       backgroundColor: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
+    }
+
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangehandler}>My hobbies: racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      )
     }
 
     return (
@@ -55,22 +74,7 @@ class App extends Component {
       <button 
         style={style}
         onClick={this.togglePersonsHandler}>Toggle Persons</button>
-      {
-        this.state.showPersons === true ? 
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Max!')}
-              changed={this.nameChangehandler}>My hobbies: racing</Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
-          </div> : null
-        }
+        {persons}
     </div>
     );
   }
