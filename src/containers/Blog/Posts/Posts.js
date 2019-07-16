6a01967192ca6c3a406f6e48208267d7cb4284af
MariusParasca <mrs.parasca@gmail.com>
2019-07-16 11:58:05 +0300

    added link component that does not reload the state

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 
 import Post from "../../../components/Post/Post";
-import './Post.css'
+import "./Post.css";
 
 class Posts extends Component {
   state = {
@@ -10,6 +10,7 @@ class Posts extends Component {
   };
 
   componentDidMount() {
+    console.log(this.props);
     axios
       .get("/posts")
       .then(response => {
