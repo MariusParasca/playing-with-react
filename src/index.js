fca31a444372a501081eeb59827a8a8d5e72c25f
MariusParasca <mrs.parasca@gmail.com>
2019-07-17 14:28:36 +0300

    Lazy loading with React Suspense

--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
-axios.defaults.headers.common['Authorization'] = 'Auth Token';
-axios.defaults.headers.post["Content-type"] = "application/json";
+import './index.css';
+import App from './App';
 
-axios.interceptors.request.use(
-  request => {
-    console.log(request);
-    // Edit request config
-    return request;
-  },
-  error => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-axios.interceptors.response.use(
-  response => {
-    console.log(response);
-    // Edit response config
-    return response;
-  },
-  error => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
