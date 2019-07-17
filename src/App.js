36c9b51a9e11d5a266ef1373184cd15e9c2ca647
MariusParasca <mrs.parasca@gmail.com>
2019-07-17 14:33:52 +0300

    app route basename

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // Adding basename of the app: <BrowserRouter basename="/my-app">
       <BrowserRouter>
         <div className="App">
           <Blog />
