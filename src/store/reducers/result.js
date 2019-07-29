d07160a32726551800e3a8470276d3101cc125b2
MariusParasca <mrs.parasca@gmail.com>
2019-07-29 17:44:10 +0300

    action creators

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,4 @@
-import * as actionTypes from "../actions/actions";
+import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
   results: []
@@ -7,11 +7,12 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
+      // Change data
       return {
         ...state,
         results: state.results.concat({
           id: new Date(),
-          value: action.result
+          value: action.result * 2
         })
       };
     case actionTypes.DELETE_RESULT:
